8d1592caaf96abcfb782c27364fc5ee778407bff
asteriskauto <mnkomov@gmail.com>
2015-07-06 12:25:01 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Управление списком</title>
+ </head>
+ <body>
+  <p><a href="add_form.php">Добавить номер в базу</a></p>
+  <p><a href="remove_form.php">Удалить номер из базы</a></p>
+  <p><a href="show.php">Показать список номеров</a></p>
+ </body>
+</html>
