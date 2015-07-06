a95ec4d5713acb4227b307a9858f3e1b8c56779d
asteriskauto <mnkomov@gmail.com>
2015-07-06 12:27:40 +0300

    Create remove_form.php

---
+++ remove_form.php
@@ -0,0 +1,12 @@
+<p>
+<table width="300" border="1" bordercolor="#630000" cellpadding="3" cellspacing="0">
+<tr><td>
+<font size="2" face="verdana,arial,georgia" color="#630000">Введите номер, который нужно удалить.</font>
+<form action="delete.php" method="post">
+<input type="text" placeholder="прим. 79261234567" name="phonedel" value="" />
+<input type="submit" name="send" value="Удалить" />
+</form>
+</td></tr>
+</table>
+</p>
+<p><a href="index.php">На главную</a></p>
