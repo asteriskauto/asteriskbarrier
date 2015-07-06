f25ced4d130a3622025b96650ab1cd558f77bc01
asteriskauto <mnkomov@gmail.com>
2015-07-06 12:25:32 +0300

    Create add_form.php

---
+++ add_form.php
@@ -0,0 +1,13 @@
+<p>
+<table width="300" border="1" bordercolor="#630000" cellpadding="3" cellspacing="0">
+<tr><td>
+<font size="2" face="verdana,arial,georgia" color="#630000">Введите имя и номер.</font>
+<form action="insert.php" method="post">
+<input type="text" placeholder="прим. Иван Петров" name="phonee" value="" />
+<input type="text" placeholder="прим. 79261234567" name="namee" value="" />
+<input type="submit" name="send" value="Отправить" />
+</form>
+</td></tr>
+</table>
+</p>
+<p><a href="index.php">На главную</a></p>
